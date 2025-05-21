89060ab7026198432857541f093d45babfdc0116
jonathan-matheus <johhn.dev@gmail.com>
2025-05-21 15:09:21 -0300

    Exibindo erros

--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -1,3 +1,3 @@
 <x-layout title="Editar Série  '{{ $serie->nome }}'">
-    <x-form action="{{ route('series.update', $serie->id) }}" nome="{{ $serie->nome }}" />
+    <x-form action="{{ route('series.update', $serie->id) }}" nome="{{ $serie->nome }}" :update="true" />
 </x-layout>
